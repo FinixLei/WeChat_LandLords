877c5bcf3974a81dc374ceaca43d47a7b694ffaf
Finix Lei <leilei4@lenovo.com>
2018-03-14 21:50:48 +0800

    Refine User Input, Make it more convenient

--- solve_puzzle.py
+++ solve_puzzle.py
@@ -2,11 +2,8 @@ from ui_engine import UIEngine
 
 
 def main():
-    # lorder_cards = ['A', 'A', 'K', 'J', 9, 9, 8, 6, 4]
-    # farmer_cards = ['2', 'A', 'J', 10, 10, 7, 7, 6, 5, 5, 4, 3, 3]
-
-    lorder_cards = ['A', 'A', 'K', 'J', 9, 9, 8, 6, 4]
-    farmer_cards = ['2', 'A', 'J', 10, 10, 6, 4, 3, 3]
+    lorder_cards = "A A K J 9 9 8 6 4".split()
+    farmer_cards = "2 A J 10 10 6 4 3 3".split()
 
     UIEngine.run(lorder_cards, farmer_cards)
 
--- test_minmax_engine.py
+++ test_minmax_engine.py
@@ -2,12 +2,8 @@ from minmax_engine import start_engine
 
 
 def main():
-    # lorder_cards = [2, 2, 'A', 'K', 10, 7, 6, 6, 5, 5, 4, 3]
-    # farmer_cards = ['Z', 2, 'A', 'K', 'Q', 'J', 10, 5, 5, 4, 3]
-    # best_move = start_engine(lorder_cards=lorder_cards, farmer_cards=farmer_cards, farmer_move=[])
-
-    lorder_cards = ['A', 'A', 'K', 'J', 9, 9, 8, 6, 4]
-    farmer_cards = ['2', 'A', 'J', 10, 10, 7, 7, 6, 5, 5, 4, 3, 3]
+    lorder_cards = "A A K J 9 9 8 6 4".split()
+    farmer_cards = "2 A J 10 10 7 7 6 5 5 4 3 3".split()
     best_move = start_engine(lorder_cards=lorder_cards, farmer_cards=farmer_cards, farmer_move=[])
     print("Best Move is %s" % best_move)
 
