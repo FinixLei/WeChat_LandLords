26563562a5ef7e0a7934fb6d62f1a88ed5ad9bca
Finix Lei <leilei4@lenovo.com>
2018-03-16 22:09:50 +0800

    Improve UI

--- solve_puzzle.py
+++ solve_puzzle.py
@@ -11,7 +11,7 @@ def main():
     # lorder_cards = "3 4 5 大王"
     # farmer_cards = "3 4 5 小王"
 
-    UIEngine.run(lorder_cards.split(), farmer_cards.split())
+    UIEngine.run(lorder_cards.split(), farmer_cards.split(), farmer_move=[])
 
 
 if __name__ == '__main__':
